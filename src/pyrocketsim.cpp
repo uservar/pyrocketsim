fd2b7897962c89ab6899f36a5adf8e601e95dfbd
uservar <userfloat@protonmail.com>
2023-03-24 03:37:11 +0100

    Add BallHitInfo

--- src/pyrocketsim.cpp
+++ src/pyrocketsim.cpp
@@ -209,16 +209,25 @@ PYBIND11_MODULE(pyrocketsim, m) {
         .def("get_state", &BoostPad::GetState)
         .def("set_state", &BoostPad::SetState, "boost_pad_state"_a);
 
+    py::class_<BallHitInfo>(m, "BallHitInfo")
+        .def(py::init<>())
+        .def_readwrite("car_id", &BallHitInfo::carID)
+        .def_readwrite("relative_pos_on_ball", &BallHitInfo::relativePosOnBall)
+        .def_readwrite("ball_pos", &BallHitInfo::ballPos)
+        .def_readwrite("extra_hit_vel", &BallHitInfo::extraHitVel)
+        .def_readwrite("tick_count_when_hit", &BallHitInfo::tickCountWhenHit);
+
     py::class_<BallState>(m, "BallState")
         .def(py::init<>())
         .def_readwrite("pos", &BallState::pos)
         .def_readwrite("vel", &BallState::vel)
-        .def_readwrite("ang_vel", &BallState::angVel);  
+        .def_readwrite("ang_vel", &BallState::angVel)
+        .def_readwrite("ball_hit_info", &BallState::ballHitInfo);
 
     py::class_<Ball>(m, "Ball")
         .def("get_state", &Ball::GetState)
         .def("set_state", &Ball::SetState, "ball_state"_a)
-        .def("get_radius", [](Ball &ball) {return ball.GetRadius() * 50;});
+        .def("get_radius", &Ball::GetRadius);
 
     py::class_<decltype(CarState::worldContact)>(m, "WorldContact")
         .def_readwrite("has_contact", &decltype(CarState::worldContact)::hasContact)

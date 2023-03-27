b13b8d191de709d8854fdb307de96b5dbf8415e6
uservar <userfloat@protonmail.com>
2023-03-27 03:37:31 +0200

    Build wheels

--- README.md
+++ README.md
@@ -1,55 +1,42 @@
 # pyrocketsim
-Python bindings for RocketSim using pybind11
-
-## Clone & Build
-```
-git clone https://github.com/uservar/pyrocketsim
-cd pyrocketsim
-cmake -B ./build -DCMAKE_BUILD_TYPE=Release
-cmake --build ./build --config Release --parallel
-```
-The built binaries will either be in ./build/ or ./build/Release/
-
-Note: As a limitation of using pybind, binaries will only work with the specific python version that was used in building them.
+Python bindings for RocketSim using Boost.Python
 
 ## Example Usage
 
 ```py
-import pyrocketsim as rs
+import pyrocketsim as RS
 
 # Initialize RocketSim (loads arena collision meshes, etc.)
-rs.init()
+RS.init()
 
 # setup rocketsim arena
 tick_rate = 120
-arena = rs.Arena(rs.SOCCAR, tick_rate)
+arena = RS.Arena(RS.SOCCAR, tick_rate)
 print(f"Arena tick rate: {arena.tick_rate}")
 
 
 # setup ball initial state
 ball_state = arena.ball.get_state()
-ball_state.pos = rs.Vec(0, 0, 500)
-ball_state.vel = rs.Vec(0, -6000, 0)  # A ball with 0 vel will not fall
+ball_state.pos = RS.Vec(0, 0, 500)
+ball_state.vel = RS.Vec(0, -6000, 0)  # A ball with 0 vel will not fall
 arena.ball.set_state(ball_state)
 print("Set ball state")
 
 # setup rocketsim cars
 for i in range(2):
-    team = rs.BLUE if i % 2 else rs.ORANGE
-    car = arena.add_car(team, rs.OCTANE)
+    team = RS.BLUE if i % 2 else RS.ORANGE
+    car = arena.add_car(team, RS.OCTANE)
     car_state = car.get_state()
     car_state.boost = 100
-    car_state.pos = rs.Vec(car.id * 200, car.id * 200, 200)
-    car_state.vel = rs.Vec(100, 100, 100)
-    car_state.ang_vel = rs.Vec(0, 0, 5.5)
+    car_state.pos = RS.Vec(car.id * 200, car.id * 200, 200)
+    car_state.vel = RS.Vec(100, 100, 100)
+    car_state.ang_vel = RS.Vec(0, 0, 5.5)
     car.set_state(car_state)
     print(f"Car added to team {team} with id {car.id}")
 
 # set car controls
 car = arena.get_cars()[0]
-car_controls = rs.CarControls()
-car_controls.throttle = 1
-car_controls.steer = -1
+car_controls = RS.CarControls(throttle=1, pitch=-1)
 car.set_controls(car_controls)
 
 # do any number of steps
@@ -67,9 +54,39 @@ arena.step(100)
 
 old_arena = arena.clone(copy_callbacks=False)
 old_arena.write_to_file("arena_file_path")
-same_old_arena = rs.Arena.load_from_file("arena_file_path")
+same_old_arena = RS.Arena.load_from_file("arena_file_path")
 
 arena.reset_to_random_kickoff()
 ```
 
-If you want an idea of what's included in the bindings you can either read the [source code](https://github.com/uservar/pyrocketsim/blob/main/src/pyrocketsim.cpp) or [pydoc pyrocketsim](https://gist.github.com/uservar/95bdfef383f691181883ddb2615be443).
+If you want an idea of what's included in the bindings you can either read the [source code](src/pyrocketsim.cpp) or [pydoc pyrocketsim](https://gist.github.com/uservar/95bdfef383f691181883ddb2615be443).
+
+
+## Clone and build from Source
+
+Make sure to clone recusively or git init/update submodules later
+```
+git clone --recursive https://github.com/uservar/pyrocketsim/
+cd pyrocketsim
+```
+
+You need to have a C++20 compiler and CMake >= 3.15 installed and in your path.
+
+Then you need to get a static version of boost, with python and numpy components. Check the [workflow file](.github/workflows/main.yml) for reference.
+
+- Then, you can install locally using pip:
+```
+pip install -e .
+```
+You can set CMAKE_BUILD_PARALLEL_LEVEL env variable to limit the number of parallel jobs.
+
+- Or, you can build directly:
+```
+cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+cmake --build ./build --config Release --parallel
+```
+In which case, the built binaries will either be in ./build/ or ./build/Release/
+
+If you want to specify the python version, set PYTHON_VERSION env variable to "3.x" or use -DPYTHON_VERSION=3.x when building directly with cmake.
+
+Note: As a limitation of using pybind, the compiled binaries will only work with the specific python version that was used in building them.
---
+++ setup.py
@@ -0,0 +1,144 @@
+"""
+This is a modified example setup.py file for pybind11 projects.
+"""
+
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+project_dir = Path("").resolve()
+
+
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+class CMakeExtension(Extension):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(project_dir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext: CMakeExtension) -> None:
+        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)  # type: ignore[no-untyped-call]
+        extdir = ext_fullpath.parent.resolve()
+
+        # Using this requires trailing slash for auto-detection & inclusion of
+        # auxiliary "native" libs
+
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        boost_root = os.environ.get("BOOST_ROOT", None)
+
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
+            f"-DBOOST_ROOT={boost_root}" if boost_root else ""
+        ]
+
+        build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator or cmake_generator == "Ninja":
+                try:
+                    import ninja
+
+                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
+                    cmake_args += [
+                        "-GNinja",
+                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
+                    ]
+                except ImportError:
+                    pass
+
+        else:
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
+                build_args += ["--config", cfg]
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # Use parallel with default number of jobs
+            build_args += [f"--parallel"]
+
+        build_dir = (Path(ext.sourcedir) / "build").resolve()
+        if not build_dir.exists():
+            build_dir.mkdir(parents=True)
+
+        build_dir = os.fspath(build_dir)
+
+        subprocess.run(
+            ["cmake", "-B", build_dir, *cmake_args], cwd=ext.sourcedir, check=True
+        )
+        subprocess.run(
+            ["cmake", "--build", build_dir, *build_args], cwd=ext.sourcedir, check=True
+        )
+
+
+# using README.md to define long_description
+with open(project_dir / "README.md") as readme_file:
+    readme = readme_file.read()
+
+# The information here can also be placed in setup.cfg - better separation of
+# logic and declaration, and simpler if you include description/version in a file.
+setup(
+    name="pyrocketsim",
+    version="1.1.0.dev2",
+    author="Zealan & uservar",
+    description="Python bindings for RocketSim",
+    long_description=readme,
+    ext_modules=[CMakeExtension("pyrocketsim")],
+    cmdclass={"build_ext": CMakeBuild},
+    zip_safe=False,
+    python_requires=">=3.6",
+)
